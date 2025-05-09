8a2fd87b522d25b38e6c1008ed90be64bf42a597
Tom Sherman <the.tomsherman@gmail.com>
2025-05-09 15:35:28 +0100

    Truer to life repro

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,7 @@ export default function Home() {
   return (
     <div>
       <div style={{ width: 100, height: 2000, background: "lightgray" }} />
-      <Link
-        href="/a"
-        scroll={false}
-        // Uncomment the following code for a possible workaround
-        // onNavigate={() => {
-        //   window.scrollTo({
-        //     top: 0,
-        //   });
-        // }}
-      >
-        Link
-      </Link>
+      <Link href="/a">Link</Link>
     </div>
   );
 }
